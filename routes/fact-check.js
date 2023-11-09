86f9b3710c74bf2839f9182ab3459f2169e64336
Nicholas Lediet <nicklediet@gmail.com>
2023-11-09 01:59:09 -0500

    Refactor to use HTTP and complete parsing of mfcb reports. Added caching

--- routes/fact-check.js
+++ routes/fact-check.js
@@ -1,20 +1,60 @@
 import { getBaseURL } from '../lib/utils.js'
-import FactCheckBot from '../lib/bots/fact-check.js'
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+let resualtCache = {}
 
 const getBiasSearchURL = baseUrl => {
     const params = new URLSearchParams({ s: baseUrl }).toString()
     return `https://mediabiasfactcheck.com/?${params}`
 }
 
+const getSourceListPage = async (url) => {
+    const { data: html } = await axios.get(url)
+    const $ = cheerio.load(html)
+    return $('article.content-list .entry-title a').attr('href')
+}
+
+const getReport = async (url) => {
+    const { data: html } = await axios.get(url)
+    const $ = cheerio.load(html)
+    const content = $('.entry-content .entry-content p:nth-of-type(2)')
+    const reportDataKeys = [
+        'biasRating',
+        'factualReporting',
+        'country',
+        'pressFreedomRank',
+        'mediaType',
+        'trafficAndPopularity',
+        'credibilityRating'
+    ]
+    const reportElementValues = content.text().split('\n')
+        .map(kvp => kvp.split(':')[1].trim());
+
+    return reportDataKeys.reduce((reportData, key, index) => {
+        reportData[key] = reportElementValues[index]
+        return reportData
+    },{})
+}
+
 export default (req, res) => {
     const { url } = req.body
     const baseURL = getBaseURL(url)
-    const searchURL = getBiasSearchURL(baseURL)
-    const factCheckBotInstance = new FactCheckBot({ url: baseURL })
-    factCheckBotInstance.setup();
 
-    res.json({
-        baseURL,
-        searchURL
-    })
+    if(resualtCache[baseURL]) return res.json({ ...resualtCache[baseURL], cached: true })
+
+    const searchURL = getBiasSearchURL(baseURL)
+
+    getSourceListPage(searchURL)
+        .then(async (searchSourceURL) => await getReport(searchSourceURL))
+        .then(reportDataJson => {
+            const response = {
+                ...reportDataJson,
+                baseURL,
+                searchURL,
+                cached: false
+            }
+            resualtCache[baseURL] = response
+            res.json(response)
+        })
 }
